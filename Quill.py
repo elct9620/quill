e40d59d6b546e726233123615755e6a65233afff
Aotokitsuruya <contact@aotoki.me>
2024-10-27 14:57:58 +0800

    chore: add compiled check to run with different user interface

--- Quill.py
+++ Quill.py
@@ -1,5 +1,16 @@
 import webview
 
+is_compiled = "__compiled__" in globals()
+
+
+def main():
+    if is_compiled:
+        webview.create_window("Quill", html="<h1>Working in progress</h1>")
+        webview.start()
+    else:
+        webview.create_window("Quill", html="<h1>Hello, World!</h1>")
+        webview.start()
+
+
 if __name__ == "__main__":
-    window = webview.create_window("Quill", html="<h1>Hello, World!</h1>")
-    webview.start()
+    main()
